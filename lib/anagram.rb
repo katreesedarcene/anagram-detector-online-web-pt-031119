b31529c8d510add51b49b3e85c5b6ee573565997
Kat B <katreesebarnes@gmail.com>
2019-04-06 10:38:23 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,13 +2,13 @@ require "pry"
 class Anagram
   attr_accessor :name
   def inititalize(word)
-    @word = word
+    @name = word
   end
   
   def match(array) 
     
   
-    array.collect {|words| words.split("").sort == word.split("").sort}
+    array.collect {|words| words.split("").sort == name.split("").sort}
     
 
   end
