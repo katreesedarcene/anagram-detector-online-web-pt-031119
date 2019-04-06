6a11526d32f9c6fe7426c77cdc192de8e506a356
Kat B <katreesebarnes@gmail.com>
2019-04-06 10:33:31 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+require "pry"
+class Anagram
+  attr_accessor :name
+  def inititalize(word)
+    @word = word
+  end
+  
+  def match(array) 
+    
+  
+    array.collect {|words| words.split("").sort == @word.split("").sort}
+    
+
+  end
+  
+  
+end
